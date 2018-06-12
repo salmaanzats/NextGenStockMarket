5ed4218269e2852b0d82661a310cee61e6d9d34d
BuddhiKavee <buddhikavee@gmail.com>
2018-06-12 16:15:17 +0530

    Game tables implementation

--- Code/Presentation/NextGenStockMarket/src/app/modules/game/game.component.ts
+++ Code/Presentation/NextGenStockMarket/src/app/modules/game/game.component.ts
@@ -5,29 +5,50 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { ToastsManager } from 'ng2-toastr';
 
 @Component({
+
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
+
 })
+
 export class GameComponent implements OnInit {
 
+  totalStock = [];
+  google = [];
+  yahoo = [];
+  amazon = [];
+  microsoft = [];
+
   constructor(private fb: FormBuilder, private router: Router, private gameService: GameService, private activatedRoute: ActivatedRoute,
+
     private toastr: ToastsManager, vcr: ViewContainerRef) {
+
     this.toastr.setRootViewContainerRef(vcr);
+
   }
 
   ngOnInit() {
+
     this.loadStockData();
+
   }
 
   loadStockData() {
+
     this.gameService.getStockData()
+
       .subscribe(data => {
-        debugger
-        console.log(data);
+        debugger;
+       this.google = data[0];
+       this.yahoo = data[1];
+       this.microsoft = data[2];
+       this.amazon = data[3];
+       
       }, error => {
         this.toastr.error("Stock data loading error", "Warning");
       })
-  }
 
+  }
+ 
 }
